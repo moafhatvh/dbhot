0cba6924627b0c311d818a45d585734c5f3bb513
menggang <menggang1109@gmail.com>
2024-03-06 15:54:03 +0800

    v4

--- 1.py
+++ 1.py
@@ -4,5 +4,11 @@ Created on Sat Jan  6 19:56:46 2024
 
 @author: 孟刚
 """
-
+ans=0
+for x in range(3,1001,2):
+    for y in range(2,x):
+        if x%y==0:
+            break
+    else: ans+=x
+print(ans+2)
 
